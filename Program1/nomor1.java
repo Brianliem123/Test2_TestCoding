f042bebade34bd59cbf755bd907da8064d07f9e2
brian <febrianliem123@gmail.com>
2020-01-07 16:56:47 +0700

    TEST CODING

---
+++ Program1/nomor1.java
@@ -0,0 +1,9 @@
+public class nomor1 {
+    public static void main(String args[]){
+        
+        for(int kg=1;kg<101;kg++){
+            int pon = kg*2;
+            System.out.println(kg +" kg = " +  pon);
+        }
+    }
+ }
